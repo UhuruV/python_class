713554947f21eedbe052bae942cf58554621d83b
UhuruV <uhuruvalary@gmail.com>
2020-07-16 16:31:32 -0400

    add existing file

--- account.py
+++ account.py
@@ -1,63 +1,62 @@
 class BankAcount:
-  bank="KCB"
   
-  def __init__(self, first_name, last_name):
+  
+  def __init__(self, first_name, last_name,bank):
     self.first_name=first_name
     self.last_name=last_name
+    self.bank=bank
     self.balance=0
+    self.phone_number=0
+    self.deposits=[]
+    self.withdrawals=[]
+    self.loans=0
     
   def account_name(self):
     name="{} account for {} {}".format(self.bank, self.first_name, self.last_name )
     return name
     
   def deposit (self, amount):
-    self.balance += amount
-    print("You have deposited {} to your account".format(amount)) 
+      if amount <=0:
+          print("Sorry you cannot deposit that amount")
+      else:
+        self.balance += amount
+        print("You have deposited {} to your account".format(amount)) 
     
      
   def get_balance(self, amount):
-    if amount > 0:
       return "Balance for {} is {}".format(self.account_name(),self.balance)
       
-    else:
-      return "Cannot Finish Operation"
     
   def withdraw(self, amount):
-    if amount > self.balance:
-      self.balance -= amount
-      print ("Insufficient Amount!!:", amount)
-      return
+    if amount <= 0:
+        print("Amount cannot be withdrawn")
+
+    elif amount > self.balance:
+        print ("Insufficient Amount!!:", amount)
       
     else:
-      print ("Withdrawal Successful, your balance is {}". format (self.balance))
-      return
+        self.balance -= amount
+        print ("Withdrawal Successful, your have withdrawn:",amount)
+        return
       
   def display(self):
     print ("Your Net Balance = {}".format(self.balance))   
     
-acc1=BankAcount ("Betty", "Njambi")
-acc2=BankAcount ("Hellen", "Ivy")
-print()
-
-acc1.deposit(1000)
-acc2.deposit(50)
-acc1.deposit(30)
-acc2.deposit(75)
-print()
-
-acc1.withdraw(500)
-acc2.withdraw(10)
-acc1.withdraw(7895)
-acc2.withdraw(500)
-print ()
-
-
-print (acc1.get_balance(1030)) 
-print (acc2.get_balance(0))
-print ()
-
-acc1.display()
-acc2.display()
-
+  def withdrawal_statement(self):
+      withdrawals=withdrawals.append(withdraw())
+      return withdraw
+  
+  def loan_given(self,amount):
+      print("You have received a loan of {}:".format(amount))
+      self.loans= self.loans + amount
+      
+  def loan_repayed(self,amount):
+      if amount >=1:
+          self.repay=self.loans-amount
+          print("You have repayed Ksh {}".format(amount))
+          print("Your loan balance is: {}".format(self.repay))
+          
+  
+      
 
 
